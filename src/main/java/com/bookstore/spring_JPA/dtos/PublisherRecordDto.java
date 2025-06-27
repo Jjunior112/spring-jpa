c655e8d438f0f5cecaec96c30df15d1a694ea90c
Juarez Gonzaga de Castro Junior <jgjunior@cargillbioenergia.com>
2025-06-27 10:41:54 -0300

    docs: criação de readme.md para documentar projeto

--- src/main/java/com/bookstore/spring_JPA/dtos/PublisherRecordDto.java
+++ src/main/java/com/bookstore/spring_JPA/dtos/PublisherRecordDto.java
@@ -4,7 +4,6 @@ package com.bookstore.spring_JPA.dtos;
 //import jakarta.validation.constraints.Size;
 
 public record PublisherRecordDto(
-        //@NotBlank(message = "O nome não pode estar em branco")
-        //@Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
+        
         String name
 ) {}
--- src/main/java/com/bookstore/spring_JPA/dtos/ReviewRecordDto.java
+++ src/main/java/com/bookstore/spring_JPA/dtos/ReviewRecordDto.java
@@ -1,7 +1,6 @@
 package com.bookstore.spring_JPA.dtos;
 
-public record ReviewRecordDto(//@NotBlank(message = "O nome não pode estar em branco")
-    //@Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
+public record ReviewRecordDto(
     String comment) 
     {}
 
